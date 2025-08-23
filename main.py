1753c6655cc9c39f8c3df56988b7526b00f066ed
mfbutner <mfbutner@ucdavis.edu>
2025-08-23 16:14:47 -0700

    Added the ability to download "all" of a survey's information to a folder

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-if __name__ == '__main__':
+import json
+import sys
+from QualtricsStudyGroupSurveys import QualtricsConnection
+from QualtricsStudyGroupSurveys import OathInformation
+
+
+def main():
     # first command line argument is the datacenter: https://iad1.qualtrics.com/
     # if two command line parameters are entered, then the second is assumed to be your API token
     # if 3 or 4 command line parameters are entered then
     # - sys.argv[2] is your oath client id
     # - sys.argv[3] is your oath client secret
     # - optionally, sys.argv[4] can be your scope, if not entered it will default to manage:all
-    import sys
-    from QualtricsStudyGroupSurveys import QualtricsConnection
-    from QualtricsStudyGroupSurveys import OathInformation
 
     match sys.argv:
         case [_, data_center, api_token]:
@@ -21,7 +24,316 @@ if __name__ == '__main__':
         case _:
             print('Incorrect number of command line arguments entered')
             exit(1)
-
-    print(qualtrics.who_am_i())
+    # print(qualtrics.who_am_i())
     # print(qualtrics.list_surveys())
     # print(qualtrics.get_survey('SURVEY ID'))
+    # questions = qualtrics.get_questions('SV_74gZzH3XX6XjZsO')
+    # with open(r'C:\Users\mfbut\PycharmProjects\QualtricsStudyGroupSurveys\template_questions.json', 'w') as template_questions:
+    #     json.dump(questions, template_questions)
+    #
+    survey_id = 'ENTER_ID_HERE'
+    dir_path = 'PATH_TO_DOWNLOAD_SURVEY_INFO_TO'
+    qualtrics.download_all_survey_attributes(survey_id, dir_path)
+
+
+def test_create_question(qualtrics_connection: QualtricsConnection):
+    survey_id = 'SV_8qA5Z7zoSCI3PKK'
+    endpoint = f'/API/v3/survey-definitions/{survey_id}/questions'
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    question = {
+        "QuestionText": "Who did you meet with during your meeting?",
+        "DefaultChoices": False,
+        "DataExportTag": "Q3.2",
+        "QuestionID": "QID1",
+        "QuestionType": "MC",
+        "Selector": "MAVR",
+        "SubSelector": "TX",
+        "DataVisibility": {
+            "Private": False,
+            "Hidden": False
+        },
+        "Configuration": {
+            "QuestionDescriptionOption": "UseText"
+        },
+        "QuestionDescription": "Who did you meet with during your meeting?",
+        "Choices": {
+            "1": {
+                "Display": "Person 1 Team1",
+                "DisplayLogic": {
+                    "0": {
+                        "0": {
+                            "LogicType": "EmbeddedField",
+                            "LeftOperand": "Team",
+                            "Operator": "EqualTo",
+                            "RightOperand": "1",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">If</span> <span class=\"LeftOpDesc\">Team</span> <span class=\"OpDesc\">Is Equal to</span> <span class=\"RightOpDesc\"> 1 </span>"
+                        },
+                        "1": {
+                            "LogicType": "PanelData",
+                            "LeftOperand": "m://Email1",
+                            "Operator": "NotEqualTo",
+                            "RightOperand": "Team1Person1@ucdavis.edu",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">And</span><span class=\"schema_desc\">Contact List</span><span class=\"select_val_desc LeftOperand_desc\">Email</span><span class=\"select_val_desc Operator_desc\">Is Not Equal to</span><span class=\"textbox_val_desc RightOperand_desc\">Team1Person1@ucdavis.edu</span>",
+                            "Conjuction": "And"
+                        },
+                        "Type": "If"
+                    },
+                    "Type": "BooleanExpression",
+                    "inPage": False
+                }
+            },
+            "2": {
+                "Display": "Person 2 Team 1",
+                "DisplayLogic": {
+                    "0": {
+                        "0": {
+                            "LogicType": "EmbeddedField",
+                            "LeftOperand": "Team",
+                            "Operator": "EqualTo",
+                            "RightOperand": "1",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">If</span> <span class=\"LeftOpDesc\">Team</span> <span class=\"OpDesc\">Is Equal to</span> <span class=\"RightOpDesc\"> 1 </span>"
+                        },
+                        "1": {
+                            "LogicType": "PanelData",
+                            "LeftOperand": "m://Email1",
+                            "Operator": "NotEqualTo",
+                            "RightOperand": "Team1Person2@ucdavis.edu",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">And</span><span class=\"schema_desc\">Contact List</span><span class=\"select_val_desc LeftOperand_desc\">Email</span><span class=\"select_val_desc Operator_desc\">Is Not Equal to</span><span class=\"textbox_val_desc RightOperand_desc\">Team1Person2@ucdavis.edu</span>",
+                            "Conjuction": "And"
+                        },
+                        "Type": "If"
+                    },
+                    "Type": "BooleanExpression",
+                    "inPage": False
+                }
+            },
+            "3": {
+                "Display": "Person 3 Team 1",
+                "DisplayLogic": {
+                    "0": {
+                        "0": {
+                            "LogicType": "EmbeddedField",
+                            "LeftOperand": "Team",
+                            "Operator": "EqualTo",
+                            "RightOperand": "1",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">If</span> <span class=\"LeftOpDesc\">Team</span> <span class=\"OpDesc\">Is Equal to</span> <span class=\"RightOpDesc\"> 1 </span>"
+                        },
+                        "1": {
+                            "LogicType": "PanelData",
+                            "LeftOperand": "m://Email1",
+                            "Operator": "NotEqualTo",
+                            "RightOperand": "Team1Person3@ucdavis.edu",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">And</span><span class=\"schema_desc\">Contact List</span><span class=\"select_val_desc LeftOperand_desc\">Email</span><span class=\"select_val_desc Operator_desc\">Is Not Equal to</span><span class=\"textbox_val_desc RightOperand_desc\">Team1Person3@ucdavis.edu</span>",
+                            "Conjuction": "And"
+                        },
+                        "Type": "If"
+                    },
+                    "Type": "BooleanExpression",
+                    "inPage": False
+                }
+            },
+            "4": {
+                "Display": "Person 4 Team 1",
+                "DisplayLogic": {
+                    "0": {
+                        "0": {
+                            "LogicType": "EmbeddedField",
+                            "LeftOperand": "Team",
+                            "Operator": "EqualTo",
+                            "RightOperand": "1",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">If</span> <span class=\"LeftOpDesc\">Team</span> <span class=\"OpDesc\">Is Equal to</span> <span class=\"RightOpDesc\"> 1 </span>"
+                        },
+                        "1": {
+                            "LogicType": "PanelData",
+                            "LeftOperand": "m://Email1",
+                            "Operator": "NotEqualTo",
+                            "RightOperand": "Team1Person4@ucdavis.edu",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">And</span><span class=\"schema_desc\">Contact List</span><span class=\"select_val_desc LeftOperand_desc\">Email</span><span class=\"select_val_desc Operator_desc\">Is Not Equal to</span><span class=\"textbox_val_desc RightOperand_desc\">Team1Person4@ucdavis.edu</span>",
+                            "Conjuction": "And"
+                        },
+                        "Type": "If"
+                    },
+                    "Type": "BooleanExpression",
+                    "inPage": False
+                }
+            },
+            "5": {
+                "Display": "Person 5 Team 1",
+                "DisplayLogic": {
+                    "0": {
+                        "0": {
+                            "LogicType": "EmbeddedField",
+                            "LeftOperand": "Team",
+                            "Operator": "EqualTo",
+                            "RightOperand": "1",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">If</span> <span class=\"LeftOpDesc\">Team</span> <span class=\"OpDesc\">Is Equal to</span> <span class=\"RightOpDesc\"> 1 </span>"
+                        },
+                        "1": {
+                            "LogicType": "PanelData",
+                            "LeftOperand": "m://Email1",
+                            "Operator": "NotEqualTo",
+                            "RightOperand": "Team1Person5@ucdavis.edu",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">And</span><span class=\"schema_desc\">Contact List</span><span class=\"select_val_desc LeftOperand_desc\">Email</span><span class=\"select_val_desc Operator_desc\">Is Not Equal to</span><span class=\"textbox_val_desc RightOperand_desc\">Team1Person5@ucdavis.edu</span>",
+                            "Conjuction": "And"
+                        },
+                        "Type": "If"
+                    },
+                    "Type": "BooleanExpression",
+                    "inPage": False
+                }
+            },
+            "6": {
+                "Display": "Person 1 Team2",
+                "DisplayLogic": {
+                    "0": {
+                        "0": {
+                            "LogicType": "EmbeddedField",
+                            "LeftOperand": "Team",
+                            "Operator": "EqualTo",
+                            "RightOperand": "2",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">If</span> <span class=\"LeftOpDesc\">Team</span> <span class=\"OpDesc\">Is Equal to</span> <span class=\"RightOpDesc\"> 2 </span>"
+                        },
+                        "1": {
+                            "LogicType": "PanelData",
+                            "LeftOperand": "m://Email1",
+                            "Operator": "NotEqualTo",
+                            "RightOperand": "Team2Person1@ucdavis.edu",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">And</span><span class=\"schema_desc\">Contact List</span><span class=\"select_val_desc LeftOperand_desc\">Email</span><span class=\"select_val_desc Operator_desc\">Is Not Equal to</span><span class=\"textbox_val_desc RightOperand_desc\">Team2Person1@ucdavis.edu</span>",
+                            "Conjuction": "And"
+                        },
+                        "Type": "If"
+                    },
+                    "Type": "BooleanExpression",
+                    "inPage": False
+                }
+            },
+            "7": {
+                "Display": "Person 2 Team 2",
+                "DisplayLogic": {
+                    "0": {
+                        "0": {
+                            "LogicType": "EmbeddedField",
+                            "LeftOperand": "Team",
+                            "Operator": "EqualTo",
+                            "RightOperand": "2",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">If</span> <span class=\"LeftOpDesc\">Team</span> <span class=\"OpDesc\">Is Equal to</span> <span class=\"RightOpDesc\"> 2 </span>"
+                        },
+                        "1": {
+                            "LogicType": "PanelData",
+                            "LeftOperand": "m://Email1",
+                            "Operator": "NotEqualTo",
+                            "RightOperand": "Team2Person2@ucdavis.edu",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">And</span><span class=\"schema_desc\">Contact List</span><span class=\"select_val_desc LeftOperand_desc\">Email</span><span class=\"select_val_desc Operator_desc\">Is Not Equal to</span><span class=\"textbox_val_desc RightOperand_desc\">Team2Person2@ucdavis.edu</span>",
+                            "Conjuction": "And"
+                        },
+                        "Type": "If"
+                    },
+                    "Type": "BooleanExpression",
+                    "inPage": False
+                }
+            },
+            "8": {
+                "Display": "Person 3 Team 2",
+                "DisplayLogic": {
+                    "0": {
+                        "0": {
+                            "LogicType": "EmbeddedField",
+                            "LeftOperand": "Team",
+                            "Operator": "EqualTo",
+                            "RightOperand": "2",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">If</span> <span class=\"LeftOpDesc\">Team</span> <span class=\"OpDesc\">Is Equal to</span> <span class=\"RightOpDesc\"> 2 </span>"
+                        },
+                        "1": {
+                            "LogicType": "PanelData",
+                            "LeftOperand": "m://Email1",
+                            "Operator": "NotEqualTo",
+                            "RightOperand": "Team2Person3@ucdavis.edu",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">And</span><span class=\"schema_desc\">Contact List</span><span class=\"select_val_desc LeftOperand_desc\">Email</span><span class=\"select_val_desc Operator_desc\">Is Not Equal to</span><span class=\"textbox_val_desc RightOperand_desc\">Team2Person3@ucdavis.edu</span>",
+                            "Conjuction": "And"
+                        },
+                        "Type": "If"
+                    },
+                    "Type": "BooleanExpression",
+                    "inPage": False
+                }
+            },
+            "9": {
+                "Display": "Person 4 Team 2",
+                "DisplayLogic": {
+                    "0": {
+                        "0": {
+                            "LogicType": "EmbeddedField",
+                            "LeftOperand": "Team",
+                            "Operator": "EqualTo",
+                            "RightOperand": "2",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">If</span> <span class=\"LeftOpDesc\">Team</span> <span class=\"OpDesc\">Is Equal to</span> <span class=\"RightOpDesc\"> 2 </span>"
+                        },
+                        "1": {
+                            "LogicType": "PanelData",
+                            "LeftOperand": "m://Email1",
+                            "Operator": "NotEqualTo",
+                            "RightOperand": "Team2Person4@ucdavis.edu",
+                            "Type": "Expression",
+                            "Description": "<span class=\"ConjDesc\">And</span><span class=\"schema_desc\">Contact List</span><span class=\"select_val_desc LeftOperand_desc\">Email</span><span class=\"select_val_desc Operator_desc\">Is Not Equal to</span><span class=\"textbox_val_desc RightOperand_desc\">Team2Person4@ucdavis.edu</span>",
+                            "Conjuction": "And"
+                        },
+                        "Type": "If"
+                    },
+                    "Type": "BooleanExpression",
+                    "inPage": False
+                }
+            }
+        },
+        "ChoiceOrder": [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+        ],
+        "Validation": {
+            "Settings": {
+                "ForceResponse": "ON",
+                "ForceResponseType": "ON",
+                "Type": "MinChoices",
+                "MinChoices": "1"
+            }
+        },
+        "GradingData": [],
+        "Language": [],
+        "NextChoiceId": 10,
+        "NextAnswerId": 1,
+        "QuestionText_Unsafe": "Who did you meet with during your meeting?"
+    },
+
+    response = qualtrics_connection.connection.post(endpoint, headers=headers, json=question)
+    with open(r'C:\Users\mfbut\PycharmProjects\QualtricsStudyGroupSurveys\dummy.json', 'w') as dummy_file:
+        json.dump(response.json(), dummy_file)
+
+
+if __name__ == '__main__':
+    main()
