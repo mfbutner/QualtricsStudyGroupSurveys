d7b039c8d2cfb9b144924422acb6377adca81758
Nolan Ching <nsching@ucdavis.edu>
2025-08-26 19:58:21 -0700

    csv contact uploads

---
+++ contact_list_upload.py
@@ -0,0 +1,99 @@
+import os
+import sys
+import pandas as pd
+import json
+from dotenv import load_dotenv
+from jsonschema import validate
+from QualtricsStudyGroupSurveys import QualtricsConnection
+
+load_dotenv()
+
+DIRECTORY_ID = os.getenv("Q_DIRECTORY_ID")
+MAILING_LIST_ID = os.getenv("Q_MAILING_LIST_ID")
+API_TOKEN = os.getenv("Q_API_TOKEN")
+DATA_CENTER = os.getenv("Q_DATA_CENTER")
+
+contact_schema = {
+    "type": "object",
+    "properties": {
+        "firstName": {"type": "string"},
+        "lastName": {"type": "string"},
+        "email": {"type": "string", "format": "email"},
+        "extRef": {"type": "string"},
+        "embeddedData": {
+            "type": "object",
+            "properties": {
+                "group": {"type": "string"}
+            },
+            "required": ["group"]
+        }
+    },
+    "required": ["email"]
+}
+
+def list_mailing_lists(qualtrics: QualtricsConnection, directory_id):
+    endpoint = f"/API/v3/directories/{directory_id}/mailinglists"
+    headers = {}
+    response = qualtrics.connection.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+def get_mailing_list_contacts(qualtrics: QualtricsConnection, directory_id, mailing_list_id):
+    endpoint = f"/API/v3/directories/{directory_id}/mailinglists/{mailing_list_id}/contacts"
+    headers = {}
+    response = qualtrics.connection.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+def add_contact_to_mailing_list(qualtrics: QualtricsConnection, directory_id, mailing_list_id, contact):
+    endpoint = f"/API/v3/directories/{directory_id}/mailinglists/{mailing_list_id}/contacts"
+    response = qualtrics.connection.post(endpoint, json=contact)
+    return response
+
+def upload_csv_to_mailing_list(qualtrics: QualtricsConnection, directory_id, mailing_list_id, csv_path) -> None:
+        dataframe = pd.read_csv(csv_path)
+        for row in dataframe.itertuples():
+            print(row)
+            email = row.Email
+            first_name = row.FirstName
+            last_name = row.LastName
+            contact = {
+                "firstName": first_name,
+                "lastName": last_name,
+                "email": email,
+                "extRef": str(row.StudentID),
+                "embeddedData": {
+                    "group": str(row.Team)
+                }
+            }
+            try:
+                validate(instance=contact, schema=contact_schema)
+            except Exception as e:
+                print(f"Insufficient data to create contact for email {email}: {e}")
+                continue
+            response = add_contact_to_mailing_list(qualtrics, directory_id, mailing_list_id, contact)
+            response.raise_for_status()
+
+def main():
+    if not sys.argv[1]:
+        print("Please provide a path to a csv as a command line argument.")
+        sys.exit(1)
+    csv_path = sys.argv[1]
+    qualtrics = QualtricsConnection(DATA_CENTER, API_TOKEN)
+    # res = get_mailing_list_contacts(qualtrics, DIRECTORY_ID, MAILING_LIST_ID)
+    res = upload_csv_to_mailing_list(qualtrics, DIRECTORY_ID, MAILING_LIST_ID, csv_path)
+
+    # new_contact = {
+    #     "firstName": "Test",
+    #     "lastName": "User",
+    #     "email": "nsching@ucdavis.edu",
+    #     "extRef": "123456",
+    #     "embeddedData": {
+    #         "group": "A"
+    #     }
+    # }
+    # res = add_contact_to_mailing_list(qualtrics, DIRECTORY_ID, MAILING_LIST_ID, new_contact)
+    print(json.dumps(res, indent=4))
+
+if __name__ == "__main__":
+    main()
