e228b9c25a1003322583c96f2318e7847548169a
Nolan Ching <nsching@ucdavis.edu>
2025-09-03 14:07:01 -0700

    filled in date names

--- QualtricsStudyGroupSurveys/display_data.py
+++ QualtricsStudyGroupSurveys/display_data.py
@@ -15,11 +15,29 @@ def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
     # TODO rename the question id columns for easier reading
     return df.rename(columns = old_to_new_col_name_map)
 
+def insert_people_names(original_df: pd.DataFrame) -> pd.DataFrame:
+    # TODO implement to replace piped texted with the embedded data people names from row
+    return original_df
+
+def populate_dates(original_df: pd.DataFrame) -> pd.DataFrame:
+    """Converts 'Date1' -> actual date"""
+    df = original_df.copy()
+    start_date = pd.to_datetime(df["__js_StartDate"]).dt.normalize()
+    for date_column in [col for col in df.columns if col.endswith("_Q3.1")]:
+        days_from_start = int(date_column[-1]) - 1
+        days_difference = pd.to_timedelta(days_from_start, unit="D")
+        df[date_column] = (start_date + days_difference).dt.strftime("%m-%d-%Y")
+    return df
+
 def format_df_for_display(original_df: pd.DataFrame) -> pd.DataFrame:
     df = original_df.copy()
     df = df[df.Finished == "True"].drop_duplicates(ignore_index=True) # Show only complete responses
 
-    cols_to_drop = ["RecipientEmail.1", "Finished"]
+    df = populate_dates(df)
+    df = insert_people_names(df)
+
+    cols_to_drop = ["RecipientEmail.1", "Finished", "CanvasId", "StudentID", 
+                    "__js_StartDate"]
     df = df.drop(columns=cols_to_drop, errors='ignore')
 
     df = reorder_columns(df)
@@ -45,21 +63,21 @@ def build_streamlit(df: pd.DataFrame):
 
     teams_options = df["Team"].dropna().astype(str).unique().tolist()
     team_choices = sorted([team for team in teams_options if team])
-    team_choices.insert(0, "All teams")
+    team_choices.insert(0, "All")
     
     team_choice = st.selectbox("Choose a team", options=team_choices, index=0) # autoselect "All teams"
 
-    if team_choice != "All teams":
+    if team_choice != "All":
         df = df.loc[df["Team"].astype(str) == team_choice].copy() # .copy() silences a pandas warning
         df.drop(columns="Team", inplace=True) # Remove team name column if already filtering for team
         df.reset_index(drop=True, inplace=True)
 
-    meeting_number_choice = st.selectbox("Filter for number of meetings", options=["0", "1+"], index=1)
+    meeting_number_choice = st.selectbox("Filter for number of meetings", options=["All", "0", "1+"], index=0)
     meet_count = pd.to_numeric(df["Q1.1"]).fillna(0)
 
     if meeting_number_choice == "0":
         df = df.loc[meet_count == 0].copy()
-    else:
+    elif meeting_number_choice == "1+":
         df = df.loc[meet_count != 0].copy() 
 
     st.dataframe(df)
