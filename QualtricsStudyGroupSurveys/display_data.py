b6d04dc216365274819101804768ddcb60d711b7
Nolan Ching <nsching@ucdavis.edu>
2025-09-03 13:33:55 -0700

    filter out 0 meetings

--- QualtricsStudyGroupSurveys/display_data.py
+++ QualtricsStudyGroupSurveys/display_data.py
@@ -4,14 +4,14 @@ from .fetch_responses import fetch_responses
 
 
 def reorder_columns(df: pd.DataFrame) -> pd.DataFrame:
-    front_col_names = ["RecordedDate", "Team", "RecipientEmail", 
+    front_col_names = ["RecipientEmail", "Team", 
                        "RecipientLastName", "RecipientFirstName"]
     remaining_col_names = [col for col in df.columns if col not in front_col_names]
     return df[front_col_names + remaining_col_names]
 
 def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
     old_to_new_col_name_map = {"RecipientEmail": "Email", "RecipientFirstName": "First Name",
-                                "RecipientLastName": "Last Name", "RecordedDate": "Date"}
+                                "RecipientLastName": "Last Name"}
     # TODO rename the question id columns for easier reading
     return df.rename(columns = old_to_new_col_name_map)
 
@@ -47,11 +47,19 @@ def build_streamlit(df: pd.DataFrame):
     team_choices = sorted([team for team in teams_options if team])
     team_choices.insert(0, "All teams")
     
-    choice = st.selectbox("Choose a team", options=team_choices, index=0) # autoselect "All teams"
+    team_choice = st.selectbox("Choose a team", options=team_choices, index=0) # autoselect "All teams"
 
-    if choice != "All teams":
-        df = df.loc[df["Team"].astype(str) == choice].copy() # .copy() silences a pandas warning
+    if team_choice != "All teams":
+        df = df.loc[df["Team"].astype(str) == team_choice].copy() # .copy() silences a pandas warning
         df.drop(columns="Team", inplace=True) # Remove team name column if already filtering for team
         df.reset_index(drop=True, inplace=True)
+
+    meeting_number_choice = st.selectbox("Filter for number of meetings", options=["0", "1+"], index=1)
+    meet_count = pd.to_numeric(df["Q1.1"]).fillna(0)
+
+    if meeting_number_choice == "0":
+        df = df.loc[meet_count == 0].copy()
+    else:
+        df = df.loc[meet_count != 0].copy() 
 
     st.dataframe(df)
