954832b92ca1d7bd256ba43dfd74e50a3550f3e8
Nolan Ching <nsching@ucdavis.edu>
2025-09-03 10:31:33 -0700

    added refresh data button

--- QualtricsStudyGroupSurveys/display_data.py
+++ QualtricsStudyGroupSurveys/display_data.py
@@ -35,10 +35,16 @@ def format_df_for_display(original_df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 def fetch_qualtrics_response_data(qualtrics, survey_id) -> pd.DataFrame:
-    @st.cache_data() # cache dataframe
+    @st.cache_data(show_spinner=False) # cache dataframe
     def load_data():
         return fetch_responses(qualtrics, survey_id)
-    raw_df = load_data()
+    
+    if st.button("Refresh Data"):
+        load_data.clear()
+        load_data()
+
+    with st.spinner("Loading response data from Qualtrics..."):
+        raw_df = load_data()
     return format_df_for_display(raw_df)
 
 def build_streamlit(df: pd.DataFrame):
@@ -48,7 +54,7 @@ def build_streamlit(df: pd.DataFrame):
     teams_options = df["Team"].dropna().astype(str).unique().tolist()
     team_choices = sorted([team for team in teams_options if team])
     team_choices.insert(0, "All teams")
-
+    
     choice = st.selectbox("Choose a team", options=team_choices, index=0) # autoselect "All teams"
 
     if choice != "All teams":
